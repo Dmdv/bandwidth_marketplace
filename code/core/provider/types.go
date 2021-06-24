1bd3966df8a5b39934126fa98521504eaae6b529
murashovven <murashovven@gmail.com>
2021-06-24 12:29:29 +0300

    Refactor acknowledgment struct

--- code/core/provider/types.go
+++ code/core/provider/types.go
@@ -4,29 +4,37 @@ import (
 	"encoding/json"
 
 	"github.com/0chain/bandwidth_marketplace/code/core/node"
+	"github.com/0chain/bandwidth_marketplace/code/core/time"
 	"github.com/0chain/bandwidth_marketplace/code/core/transaction"
 )
 
 type (
 	// Acknowledgment represents accepting Provider's terms by Consumer.
 	Acknowledgment struct {
-		ID            string `json:"id"`
-		ConsumerID    string `json:"consumer_id"`
-		ProviderID    string `json:"provider_id"`
-		AccessPointID string `json:"access_point_id"`
-		SessionID     string `json:"session_id"`
+		AccessPointID string        `json:"access_point_id"`
+		ConsumerID    string        `json:"consumer_id"`
+		ProviderID    string        `json:"provider_id"`
+		SessionID     string        `json:"session_id"`
+		ProviderTerms ProviderTerms `json:"provider_terms"`
 	}
 
-	// Provider represents provider's node type which stores in blockchain.
+	// Provider represents providers node stored in block chain.
 	Provider struct {
-		ID    string `json:"id"`
-		Terms Terms  `json:"terms"`
+		ID    string        `json:"id"`
+		Terms ProviderTerms `json:"terms"`
 	}
 
-	// Terms represents information of Provider's services.
+	// ProviderTerms represents information of provider and services terms.
+	ProviderTerms struct {
+		Terms
+		QoS QoS `json:"qos"`
+	}
+
+	// Terms represents information of provider terms.
 	Terms struct {
-		Price int64 `json:"price"` // per MB
-		QoS   QoS   `json:"qos"`
+		Price     int64          `json:"price"`      // per byte
+		Volume    int64          `json:"volume"`     // in bytes
+		ExpiredAt time.Timestamp `json:"expired_at"` // valid till
 	}
 
 	// QoS represents a Quality of Service and contains uploading and downloading speed
@@ -65,7 +73,6 @@ func ExecuteAcceptTerms(provID, apID, sessID string) (*Acknowledgment, error) {
 		return nil, err
 	}
 
-	ackn.ID = txnHash
 	ackn.ConsumerID = node.GetSelfNode().ID()
 	return &ackn, err
 }
